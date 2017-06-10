e1cd614aad050f3be7b82cf935cbcc96a8f35858
juanguo999 <juanguo999@gmail.com>
2017-06-11 10:18:55 +1200

    layout change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/blogs') }}">Blogs</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
@@ -79,10 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Skill Test
                 </div>
 
                 <div class="links">
+                    @if (Auth::check())
+                    <a href="https://laravel.com/docs">Blogs</a>
+                    @endif
                     <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
